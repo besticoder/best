f11421723106e3e071ba714a31561e64b777564a
vikram vanaliya <vikraminphp@gmail.com>
2019-06-25 17:36:01 +0530

    add db and user seeds

--- database/seeds/CarTableSeeder.php
+++ database/seeds/CarTableSeeder.php
@@ -11,18 +11,24 @@ class CarTableSeeder extends Seeder
      */
     public function run()
     {
-    	for($i=0; $i < 10000; $i++)
+    	for($i=0; $i < 1000; $i++)
     	{
     		DB::table('cars')->insert([
 	            'car_type' => 'Petrol',
                 'car_img' => '',
                 'car_name' => Str::random(10),
 	            'car_model_no' => Str::random(10),
-	            'car_color' => '#'.Str::random(6),
+	            'car_color' => Str::random(6),
 	            'car_company' => Str::random(10),
 	            'created_at' => date('Y-m-d h:i:s'),
 	            'updated_at' => date('Y-m-d h:i:s'),
 	        ]);
     	}
+
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('12345678'),
+        ]);
     }
 }
